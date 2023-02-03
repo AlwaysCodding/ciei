f533c7beda9845b4367aba3de74f440a06779197
Igor Vitor <igorvitor.iv@gmail.com>
2023-02-02 21:33:59 -0300

    Reativando o mapa e ajustando texto

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,7 @@
 let map;
 
 function initMap() {
-    const myLatLng = { lat: -14.815257, lng: -39.2902629 }
+    const myLatLng = { lat: -14.798614153727822, lng: -39.170296909176216 }
     const map = new google.maps.Map(document.getElementById("mapa"), {
         center: myLatLng,
         zoom: 15,
@@ -10,11 +10,11 @@ function initMap() {
     const marker = new google.maps.Marker({
         position: myLatLng,
         map,
-        title: "Hello World!",
+        title: "Evento CIEI",
     });
 
     const contentString =
-        '<div id="content"><img src="ativos/logo.jpeg" alt="logo" style="height: 100px;width: 200px;"></div>';
+        '<div id="content"><h4>I CONGRESSO DE CIÊNCIAS DA EDUCAÇÃO: CONSTRUINDO PROPOSTAS NA PESQUISA, ENSINO E NA EXTENSÃO CCE – UESC / 2023</h4><img src="ativos/logo.jpeg" alt="logo" style="height: 75px;width: 150px;"></div>';
 
     const infowindow = new google.maps.InfoWindow({
         content: contentString,
